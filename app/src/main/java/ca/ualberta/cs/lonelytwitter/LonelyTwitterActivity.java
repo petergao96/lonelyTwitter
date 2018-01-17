9232433f315d41a9060d0dd9fde29cdf041d39c2
Haitian Gao <haitian@cs.ualberta.ca>
2018-01-16 19:29:33 -0700

    tweets

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/LonelyTwitterActivity.java
@@ -12,11 +12,13 @@ import java.util.Date;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
 public class LonelyTwitterActivity extends Activity {
 
@@ -28,11 +30,14 @@ public class LonelyTwitterActivity extends Activity {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		Log.i("LifeCycle ---->", "onCreate is called");
 		setContentView(R.layout.main);
 
 		bodyText = (EditText) findViewById(R.id.body);
 		Button saveButton = (Button) findViewById(R.id.save);
 		oldTweetsList = (ListView) findViewById(R.id.oldTweetsList);
+
+
 
 		saveButton.setOnClickListener(new View.OnClickListener() {
 
@@ -48,12 +53,60 @@ public class LonelyTwitterActivity extends Activity {
 
 	@Override
 	protected void onStart() {
+
 		// TODO Auto-generated method stub
 		super.onStart();
+		Log.i("LifeCycle --->", "onStart is called");
 		String[] tweets = loadFromFile();
 		ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
 				R.layout.list_item, tweets);
 		oldTweetsList.setAdapter(adapter);
+
+        NormalTweet normalTweet = new NormalTweet("");
+        try {
+            normalTweet.setMessage("Hello World!");
+//            normalTweet.setMessage("aaaaaaaa123904ifkdjfhirhtiorhtherihgtjkerhgjkhergtjkherjkh49ryhui4thg754tdfjlsdkhioerhgiohruioghejiogfuioerhgfiohrjkfhasdjkhgiuhgiorgjkdfhgidfgsdkljfkldsjfklsdjklfjdklsfjkldghsk");
+        }
+        catch (TweetTooLongException e) {
+            Log.e("Error ---->", "Tweet message too long");
+
+        }
+
+
+        ImportantTweet importantTweet1 = new ImportantTweet("Hello World! This is important");
+        ImportantTweet importantTweet2 = new ImportantTweet("This is another important tweet");
+
+        NormalTweet normalTweet1 = new NormalTweet("This is not that important");
+        NormalTweet normalTweet2 = new NormalTweet("This is not that important either");
+
+        ArrayList <Tweet> tweetList = new ArrayList<Tweet>();
+        tweetList.add(normalTweet);
+        tweetList.add(normalTweet1);
+        tweetList.add(normalTweet2);
+        tweetList.add(importantTweet1);
+        tweetList.add(importantTweet2);
+
+        for (Tweet t:
+                tweetList) {
+            Log.d("Tweet Polymorphism", t.isImportant().toString());
+
+        }
+
+
+        ArrayList <Tweetable> tweetableList = new ArrayList<Tweetable>();
+        tweetableList.add(normalTweet);
+        tweetableList.add(normalTweet1);
+        tweetableList.add(normalTweet2);
+        tweetableList.add(importantTweet1);
+        tweetableList.add(importantTweet2);
+
+        String messageOnScreen = "";
+        for (Tweetable t:
+             tweetableList) {
+            messageOnScreen += t.getMessage() + "\n";
+        }
+        Toast.makeText(this, messageOnScreen, Toast.LENGTH_SHORT).show();
+
 	}
 
 	private String[] loadFromFile() {
@@ -91,5 +144,11 @@ public class LonelyTwitterActivity extends Activity {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+	}
+
+	@Override
+	protected void onDestroy() {
+		super.onDestroy();
+		Log.i("Lifecycle", "onDestroy is called");
 	}
 }
