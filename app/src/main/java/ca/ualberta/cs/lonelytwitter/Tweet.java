09a83a11cf04f86918fe531b1dc0abc793e21d85
Haitian Gao <haitian@cs.ualberta.ca>
2018-01-30 18:44:57 -0700

    Comments

--- app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
+++ app/src/main/java/ca/ualberta/cs/lonelytwitter/Tweet.java
@@ -1,14 +1,41 @@
+
+
+
+/*
+ * Tweet
+ *
+ * Version 1.0
+ *
+ * January 30, 2018
+ *
+ * Copyright © 2018 Team X, CMPUT301, University of Alberta -- All Rights Reserved.
+ * You may use, distribute, or modify this code under terms and conditions of the Code of Student Behaviour at university of Alberta
+ * You can find a copy of the licence in this project. Otherwise please contact contact@abc.ca.
+ */
+
 package ca.ualberta.cs.lonelytwitter;
 
 import java.util.Date;
 
 /**
- * Created by dezfuli on 1/16/18.
+ *  Tweet class represents tweets
+ *  @author dezfuli
+ *  @version 1.0
+ *  @see NormalTweet
+ *  @see ImportantTweet
  */
+
+
 
 public abstract class Tweet implements Tweetable {
     private String message;
     private Date date;
+
+    /**
+     * Constructs a tweet object without a date
+     *
+     * @param message
+     */
 
     Tweet(String message){
 
@@ -17,14 +44,34 @@ public abstract class Tweet implements Tweetable {
 //        message = message;
     }
 
+    /**
+     * Constructs a tweet object
+     *
+     * @param message tweet message
+     * @param date tweet date
+     */
+
     Tweet(String message, Date date){
         this.message = message;
         this.date = date;
     }
 
+    /**
+     * Gets a string message
+     *
+     * @return message as a String
+     */
+
     public String getMessage(){
         return message;
     }
+
+    /**
+     * Sets tweet message
+     *
+     * @param message tweet message
+     * @throws TweetTooLongException The tweet is over 140 characters
+     */
 
     public void setMessage(String message) throws TweetTooLongException{
         if (message.length() < 140){
